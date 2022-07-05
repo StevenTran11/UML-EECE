33a53a30756c2b1d50f6295f3b115b958ccbb687
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2022-07-05 11:41:48 -0400

    Added Operators

---
+++ Data Structures/Time/Time.h.cpp
@@ -0,0 +1,56 @@
+/*
+ * EECE.3220: Data Structures
+ * Instructor: M. Geiger
+ * 
+ * Time.h: Time class definition
+ * Includes prototypes for overloaded operators to be written for Program 3
+ */
+// Header guard directives--ensures header included only once
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class Time {
+public:
+	/*** FUNCTIONS DEFINED FOR YOU IN Time.cpp ***/
+	Time();											// Default constructor
+	Time(unsigned h, unsigned m, char AP);			// Parameterized constructor
+	void set(unsigned h, unsigned m, char AP);		// Set time data members
+	void display(ostream &out);						// Print time to desired output stream
+	void advance(unsigned h, unsigned m);			// Advance time by given number of hours and minutes
+	bool lessThan(const Time &rhs);					// Returns true if calling object is less than argument
+
+	/*** OVERLOADED OPERATORS TO BE ADDED FOR PROGRAM 3 ***/
+	// Input/output operators
+	friend ostream &operator <<(ostream& out, const Time& rhs);
+	friend istream &operator >>(istream& in, Time& rhs);
+
+	// Comparison operators
+	bool operator ==(const Time& rhs);
+	bool operator !=(const Time& rhs);
+	bool operator <(const Time& rhs);
+	bool operator >(const Time& rhs);
+
+	// Arithmetic operators
+	Time operator +(const Time& rhs);
+	Time operator -(const Time& rhs);
+	Time& operator +=(const Time& rhs);
+	Time& operator -=(const Time& rhs);
+
+	// Increment operators--adds 1 minute to current time
+	Time& operator++();
+	Time operator++(int);
+	/*** END OVERLOADED OPERATORS TO BE ADDED FOR PROGRAM 3 ***/
+	
+
+private:
+	int hours, minutes;			// Time in hrs/mins (combined with AM/PM)
+	int miltime;				// Military (24-hour) time equivalent
+	char AMorPM;				// Indicates morning/afternoon--'A' for AM, 'P' for PM
+	
+};
+
+#endif		// TIME_H
