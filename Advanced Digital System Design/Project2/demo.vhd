1bc82483ebeffa983f90cac989914661d319f790
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2024-03-25 04:42:03 -0400

    Updated to output pic

---
+++ Advanced Digital System Design/Project2/demo.vhd
@@ -0,0 +1,38 @@
+use std.textio.all;
+
+entity make_ppm is
+end entity make_ppm;
+
+architecture demo of make_ppm is
+begin
+
+	write_file: process is
+		file out_file: text open write_mode is "my_picture_file.ppm";
+		variable out_line: line;
+		variable r, g, b: natural range 0 to 15;
+	begin
+		-- magic number
+		write(out_file, "P3" & LF);
+		-- resolution
+		write(out_file, "40 20" & LF);
+		-- maximum color
+		write(out_file, "15" & LF);
+
+		for j in 0 to 19 loop
+			for i in 0 to 39 loop
+				r := i mod 16;
+				g := (i + j) mod 16;
+				b := (15 - j) mod 16;
+				-- write pixel to file
+				write(out_line,
+						integer'image(r) & " "
+							& integer'image(g) & " "
+							& integer'image(b));
+				writeline(out_file, out_line);
+			end loop;
+		end loop;
+		
+		wait;
+	end process write_file;
+
+end architecture demo;
--- Advanced Digital System Design/Project2/sim/tb_project2.vhd
+++ Advanced Digital System Design/Project2/sim/tb_project2.vhd
@@ -1,5 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.textio.all;
 
 library ads;
 use ads.ads_fixed.all;
@@ -11,8 +13,8 @@ use vga.vga_data.all;
 entity tb_project2 is
 	generic (
 				total_stages : natural := 25;  -- Adjust the number of pipeline stages as needed
-				entities     : natural := 4;
-				vga_res: vga_timing := vga_res_default;
+				entities     : natural := 5;
+				vga_res: vga_timing := vga_res_1920x1080;
 				threshold : ads_sfixed := to_ads_sfixed(4)
 			);
 end entity tb_project2;
@@ -52,30 +54,62 @@ begin
 
 	dut: project2
 		generic map (
-			total_stages => 25;  -- Adjust the number of pipeline stages as needed
-	        entities =>		4;
-	        vga_res: =>		vga_res_default;
+			total_stages => 25,  -- Adjust the number of pipeline stages as needed
+	        entities =>		5,
+	        vga_res =>		vga_res,
 			threshold => 	to_ads_sfixed(4)
 		)
 		port map (
-			mode_in => mode_in,
-			reset => reset,
-			vga_clock => vga_clock,
+			mode_in =>		mode_in,
+			reset =>		reset,
+			vga_clock => 	vga_clock,
 			vga_red =>		vga_red,
 			vga_green =>	vga_green,
 			vga_blue =>		vga_blue,
-			h_syncff => h_syncff,
-			v_syncff => v_syncff
+			h_syncff => 	h_syncff,
+			v_syncff => 	v_syncff
 		);
 
 	stimulus: process is
+		file out_file1: text open write_mode is "test1.ppm";
+		file out_file2: text open write_mode is "test2.ppm";
+		variable out_line: line;
 	begin
+		-- magic number
+		write(out_file1, "P3" & LF);
+		-- resolution
+		write(out_file1, integer'image(timing_range(vga_res,horizontal)) & " " & integer'image(timing_range(vga_res,vertical)) & LF);
+		-- maximum color
+		write(out_file1, "15" & LF);
+
 		reset <= '0';
+		mode_in <= '0';
 		wait until rising_edge(vga_clock);
 		reset <= '1';
 
-		for i in 0 to timing_range(vga_res) loop
+		for i in 0 to timing_range(vga_res,horizontal) * timing_range(vga_res,vertical) loop
 			wait until rising_edge(vga_clock);
+			-- write pixel to file
+			write(out_line, integer'image(to_integer(unsigned(vga_red))) & " " & integer'image(to_integer(unsigned(vga_green))) & " " & integer'image(to_integer(unsigned(vga_blue))));
+			writeline(out_file1, out_line);
+		end loop;
+
+		write(out_file2, "P3" & LF);
+		-- resolution
+		write(out_file2, integer'image(timing_range(vga_res,horizontal)) & " " & integer'image(timing_range(vga_res,vertical)) & LF);
+		-- maximum color
+		write(out_file2, "15" & LF);
+
+		reset <= '0';
+		mode_in <= '1';
+		wait until rising_edge(vga_clock);
+		reset <= '1';
+
+		for i in 0 to timing_range(vga_res,horizontal) * timing_range(vga_res,vertical) loop
+			wait until rising_edge(vga_clock);
+			-- write pixel to file
+			write(out_line, integer'image(to_integer(unsigned(vga_red))) & " " & integer'image(to_integer(unsigned(vga_green))) & " " & integer'image(to_integer(unsigned(vga_blue))));
+			writeline(out_file2, out_line);
 		end loop;
 
 		test_done <= true;
