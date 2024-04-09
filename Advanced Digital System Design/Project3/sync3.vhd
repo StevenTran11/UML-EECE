fe532f5b328efd247e1bb299a81211db744c92e4
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2024-04-09 14:54:10 -0400

    2 clock flip flop

--- Advanced Digital System Design/Project3/sync3.vhd
+++ Advanced Digital System Design/Project3/sync3.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity sync3 is
   port (
-    clk : in  signal logic;
+    clk1 : in  signal logic;
+	clk2 : in  signal logic;
     rst_n : in  signal logic;
     d : in  signal logic;
     q : out signal logic
@@ -16,19 +17,27 @@ architecture rtl of sync3 is
 
 begin
 
-  process(clk, rst_n)
+  process(clk1, rst_n)
   begin
-    if rising_edge(clk) then
-      if not rst_n then
+    if rising_edge(clk1) then
+    	if not rst_n then
         q <= '0';
         q1 <= '0';
-        q2 <= '0';
       else
-        q <= q2;
-        q1 <= q;
-        q2 <= d;
+        q <= q1;
+        q1 <= q2;
       end if;
     end if;
   end process;
 
+  process(clk2, rst_n)
+  begin
+    if rising_edge(clk2) then
+    	if not rst_n then
+        q2 <= '0';
+      else
+        q2 <= d;
+      end if;
+    end if;
+  end process;
 end architecture rtl;
