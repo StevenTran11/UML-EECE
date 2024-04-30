b2c0248dd24050ec927df5ed822af83c67110bf6
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2024-04-30 15:54:07 -0400

    adding in function to check for increment

--- Advanced Digital System Design/Project3/consumer_fsm.vhd
+++ Advanced Digital System Design/Project3/consumer_fsm.vhd
@@ -8,6 +8,7 @@ entity consumer_fsm is
     );
     port (
         clk             : in  std_logic;
+		  rst          	: in  std_logic;  -- Reset
         head_ptr        : in  natural range 0 to 2**ADDR_WIDTH - 1;
         address_a       : out natural range 0 to 2**ADDR_WIDTH - 1
     );
@@ -17,23 +18,40 @@ architecture fsm_arch of consumer_fsm is
     type state_type is (WAIT_FOR_SPACE, INCREMENT_ADDRESS);
     signal state : state_type := WAIT_FOR_SPACE; -- Initialize to WAIT_FOR_SPACE
     signal next_address_a : natural range 0 to 2**ADDR_WIDTH := 2**ADDR_WIDTH - 1; -- Initialize to max value
+	 
+	 function can_advance (
+			head, tail: in natural range 0 to 2**ADDR_WIDTH - 1
+		) return boolean
+		is
+	begin
+		if tail > head and head /= 0 and tail /= 2**ADDR_WIDTH - 1 then
+			return true;
+		elsif tail < head and (head - tail) > 1 then
+			return true;
+		end if;
+		return false;
+	end function can_advance;
+	 
 begin
 
-    process(clk)
+    process(clk, rst)
     begin
-        if rising_edge(clk) then
+		  if rst = '0' then
+				state <= WAIT_FOR_SPACE;
+				next_address_a <= 0;
+        elsif rising_edge(clk) then
             case state is
                 when WAIT_FOR_SPACE =>
                     next_address_a <= next_address_a;
                 when INCREMENT_ADDRESS =>
-                    if next_address_a + 1 <= 2**ADDR_WIDTH - 1 then
-                        next_address_a <= next_address_a + 1;
-                    else
+                    if next_address_a = 2**ADDR_WIDTH - 1 then
                         next_address_a <= 0;
+                    else
+                        next_address_a <= next_address_a + 1;
                     end if;
             end case;
 
-            if (head_ptr > next_address_a + 1) or (next_address_a - head_ptr) < (2**ADDR_WIDTH - 1) then
+            if can_advance(head_ptr,next_address_a) then
                 state <= INCREMENT_ADDRESS;
             else
                 state <= WAIT_FOR_SPACE;
--- Advanced Digital System Design/Project3/producer_fsm.vhd
+++ Advanced Digital System Design/Project3/producer_fsm.vhd
@@ -12,6 +12,7 @@ entity producer_fsm is
         address_b    : out natural range 0 to 2**ADDR_WIDTH - 1;
         soc          : out std_logic;  -- Start of conversion
         done         : in  std_logic;  -- Done
+		  rst          : in  std_logic;  -- Reset
         save         : out  std_logic  -- Save
     );
 end entity producer_fsm;
@@ -20,33 +21,55 @@ architecture fsm_arch of producer_fsm is
     type state_type is (START, WAITS, CHECK, INCREMENT);
     signal state : state_type := START; -- Initialize to START
     signal next_address_b : natural range 0 to 2**ADDR_WIDTH := 0; -- Initialize to 0
+	 
+	function can_advance (
+			head, tail: in natural range 0 to 2**ADDR_WIDTH - 1
+		) return boolean
+		is
+	begin
+		if head > tail and head /= 2**ADDR_WIDTH - 1 and tail /= 0 then
+			return true;
+		elsif head < tail and (tail - head) > 1 then
+			return true;
+		end if;
+		return false;
+	end function can_advance;
+	 
 begin
 
-    process(clk)
+    process(clk, rst)
     begin
-        if rising_edge(clk) then
+		  if rst = '0' then
+				state <= START;
+				next_address_b <= 0;
+        elsif rising_edge(clk) then
             case state is
                 when START =>
                     save <= '0';
                     soc <= '1';
                     state <= WAITS;
                 when WAITS =>
-                    soc <= '0';
+						  save <= '0';
+                    soc <= '1';
                     if done = '1' then
                         state <= CHECK;
                     end if;
                 when CHECK =>
-                    if next_address_b > tail_ptr or next_address_b < tail_ptr - 1 then
+						  save <= '0';
+						  soc <= '0';
+                    if can_advance(next_address_b, tail_ptr) then
                         state <= INCREMENT;
                     else
                         state <= CHECK;
                     end if;
                 when INCREMENT =>
+						  soc <= '0';
                     save <= '1';
-                    if next_address_b + 1 <= 2**ADDR_WIDTH - 1 then
-                        next_address_b <= next_address_b + 1;
-                    else
+						  state <= START;
+                    if next_address_b = 2**ADDR_WIDTH - 1 then
                         next_address_b <= 0;
+                    else
+                        next_address_b <= next_address_b + 1;
 					end if;
             end case;
         end if;
